19f22c2661061956c8069a5fa168f9dfdb77fdc3
“banban9999” <patariro1000@gmail.com>
2023-08-06 20:50:38 +0200

    Clean up the directory structure
    Signed-off-by: “banban9999” <“patariro1000@gmail.com”>

--- bench.sh
+++
@@ -1,3 +0,0 @@
-#!/usr/bin/bash
-
-/home/isucon/benchmarker/bin/benchmarker -target localhost:8000
--- restart.sh
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/bash
-
-cd /home/isucon/webapp/go/
-
-make all
-sudo systemctl restart isucholar.go
-sleep 3
